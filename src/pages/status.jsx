6b1f5a2303e070a99a5cb40ad4f9c9ad2426a617
Haathim_wso2 <haathimmunas@gmail.com>
2022-11-16 13:46:03 +0530

    Added import for useEffect.

--- src/pages/status.jsx
+++ src/pages/status.jsx
@@ -2,7 +2,9 @@ import '../styles/status.css';
 import TopBar from '../components/topbar';
 import Side from '../components/side';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { setRef } from '@mui/material';
+import { useAuthContext } from "@asgardeo/auth-react";
 
 
 export default function Status() {
@@ -14,6 +16,7 @@ export default function Status() {
   const [identityCheck, setidentityCheck] = useState(msg);
   const [addressCheck, setaddressCheck] = useState(msg);
   const [policeCheck, setpoliceCheck] = useState(msg);
+  const {getBasicUserInfo} = useAuthContext();
 
 
   useEffect(() => {
@@ -21,6 +24,40 @@ export default function Status() {
     // 1. check if identity check is ok
       // 2. check if address check is ok
       // 3. check if police check is ok
+    async function doChecks() {
+
+      const basicUserInfo = await getBasicUserInfo();
+      const email = basicUserInfo.email;
+
+      try {
+
+        let identityResp = await fetch("/identityAPI/" + email);
+        let identityRespVal = identityResp.json();
+
+        if(identityResp === true){
+          let addressResp = await fetch("/addressAPI" + email);
+          let addressRespVal = addressResp.json()
+          setaddressCheck(addressRespVal)
+
+          let policeResp = await fetch("/policeAPI" + email)
+          let policeRespVal = await policeResp.json()
+          setpoliceCheck(policeRespVal)
+        }
+        else{
+          setidentityCheck("Identity Not Valid")
+          setaddressCheck("Please verify identity first")
+          setpoliceCheck("Please verify identity first")
+        }
+        
+      } catch (error) {
+        setidentityCheck("Error occured!!!")
+        setaddressCheck("Error occured!!!")
+        setpoliceCheck("Error occured!!!")
+      }
+
+    }
+
+    doChecks();
 
   }, []); 
 
