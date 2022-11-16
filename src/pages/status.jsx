cea60dd62ef66354fc386d37079db8139ab47d52
Haathim_wso2 <haathimmunas@gmail.com>
2022-11-16 11:36:36 +0530

    Added states for all the checks.

--- src/pages/status.jsx
+++ src/pages/status.jsx
@@ -2,9 +2,29 @@ import '../styles/status.css';
 import TopBar from '../components/topbar';
 import Side from '../components/side';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 
 export default function Status() {
+
+  let msg = "loading.."
+
+  const [name, setname] = useState(msg);
+  const [NIC, setNIC] = useState(msg);
+  const [identityCheck, setidentityCheck] = useState(msg);
+  const [addressCheck, setaddressCheck] = useState(msg);
+  const [policeCheck, setpoliceCheck] = useState(msg);
+
+
+  useEffect(() => {
+    
+    // 1. check if identity check is ok
+      // 2. check if address check is ok
+      // 3. check if police check is ok
+
+  }, []); 
+
+
   return (
     <>
     <TopBar />
@@ -14,11 +34,11 @@ export default function Status() {
             <form>
                 <div className='st-content'>
                     <h2>Application Status</h2>
-                    <p>Name</p>
-                    <p>NIC or Passport No</p>
-                    <p>Identity Check status</p>
-                    <p>Police Check status</p>
-                    <p>Address Check status</p>
+                    <p>Name: {name}</p>
+                    <p>NIC or Passport No: {NIC}</p>
+                    <p>Identity Check status: {identityCheck}</p>
+                    <p>Police Check status: {policeCheck}</p>
+                    <p>Address Check status: {addressCheck}</p>
                     <p>if all checks are approved</p>
                 </div>
                 <a href="#" type='submit'>Get your Grama Certificate</a>
