1bde0eabad293ffeda35cb4e1185d6a8e185dea3
Haathim_wso2 <haathimmunas@gmail.com>
2022-11-18 14:50:32 +0530

    .

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -57,7 +57,7 @@ export default function TopBar() {
         .then((response) => response.json())
         .then((resJson) => localStorage.setItem("API_TOKEN",JSON.stringify(resJson)))
         .then(() =>
-          fetch("https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-dev.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId", {
+          fetch("/", {
             headers: {
               Authorization: "Bearer " + JSON.parse(localStorage.getItem("API_TOKEN")).access_token
             },
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = {
-    target: 'https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0',
+    target: 'https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId',
     changeOrigin: true
 }
 module.exports = function(app) {
