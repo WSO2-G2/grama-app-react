2f66d0b76f217b83bcb623d045c9e782e037e20e
Haathim_wso2 <haathimmunas@gmail.com>
2022-11-18 14:44:05 +0530

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const proxy = {
-    target: 'https://www.google.com',
+    target: 'https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0',
     changeOrigin: true
 }
 module.exports = function(app) {
   app.use(
-    '/search',
+    '/',
     createProxyMiddleware(proxy)
   );
 };
