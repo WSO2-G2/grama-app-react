68210f4a74731409e950cfef6869c6a9f1c0ef45
malithiperera <malithiperera1998@gmail.com>
2022-11-21 13:54:22 +0530

    test

--- src/pages/nic.jsx
+++ src/pages/nic.jsx
@@ -50,7 +50,7 @@ export default function NIC() {
 
 
     }).then((response) => {
-      console.log("ID Check",response.body.data)
+      console.log("ID Check",response.data.body)
       if (response.data.body == 'true') {
         setState(1);
         axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/policeccheck/1.0.0/getalldetails', {
