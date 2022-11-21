ad1f99f101fc2634e87afefdeec16e838ed87bc3
malithiperera <malithiperera1998@gmail.com>
2022-11-21 12:39:25 +0530

    new updates

--- src/pages/nic.jsx
+++ src/pages/nic.jsx
@@ -49,11 +49,27 @@ var newid=nic.toString();
 
      
     }).then((response) => {
-      if(response.data.body){
-        console.log("Malithi")
+      if(response.data.body=='true'){
+        setState(1);
+        axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/policeccheck/1.0.0/getalldetails',{ 
+          params:{
+           'nic':`${newid}`}, 
+           
+           headers: {
+               'Authorization': `Bearer ${accessToken}`,
+             
+           }
+   
+   
+        
+       }).then((response)=>{
+console.log(response.data.body)
+       });
+
+        
       }
       else{
-        console.log("hahahha")
+        
       }
    console.log(response.data.body)
   })
