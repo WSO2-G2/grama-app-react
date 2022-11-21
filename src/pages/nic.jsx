b6daa269104f7531747a342bd96df8b8233ac283
malithiperera <malithiperera1998@gmail.com>
2022-11-21 11:58:57 +0530

    change

--- src/pages/nic.jsx
+++ src/pages/nic.jsx
@@ -36,7 +36,8 @@ export default function NIC() {
       const accessToken=JSON.parse(localStorage.getItem("API_TOKEN")).access_token;
     
        axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId',{ 
-       params:{"nic":nic}, 
+       params:{
+        'nic':'9878'}, 
         
         headers: {
             'Authorization': `Bearer ${accessToken}`,
