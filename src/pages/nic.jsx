a33f7b74402b2ac0292157620b913d487fead04e
malithiperera <malithiperera1998@gmail.com>
2022-11-21 12:43:07 +0530

    new

--- src/pages/nic.jsx
+++ src/pages/nic.jsx
@@ -10,99 +10,101 @@ import { useAuthContext } from "@asgardeo/auth-react";
 
 
 export default function NIC() {
-    const [nic, setNic] = useState('');
-    const [statestep,setState]=useState(0)
-    const [currentStatus,setCurrentStatus]=useState('pending')
+  const [nic, setNic] = useState('');
+  const [statestep, setState] = useState(0)
+  const [currentStatus, setCurrentStatus] = useState('pending')
 
-    const {
-      state,
-      signIn,
-      signOut,
-      getBasicUserInfo,
-      getIDToken,
-      getDecodedIDToken,
-      on
-    } = useAuthContext();
+  const {
+    state,
+    signIn,
+    signOut,
+    getBasicUserInfo,
+    getIDToken,
+    getDecodedIDToken,
+    on
+  } = useAuthContext();
 
-    const changenic = (e) => {
-        setNic(e.target.value)
-
-
-        //console.log(nic)
-    }
-
-    const submitID = () => {
-      
-var newid=nic.toString();
-      console.log("Testing 2",state.email)
-      const accessToken=JSON.parse(localStorage.getItem("API_TOKEN")).access_token;
-    
-       axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId',{ 
-       params:{
-        'nic':`${newid}`}, 
-        
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-          
-        }
+  const changenic = (e) => {
+    setNic(e.target.value)
 
 
-     
+    //console.log(nic)
+  }
+
+  const submitID = () => {
+
+    var newid = nic.toString();
+    console.log("Testing 2", state.email)
+    const accessToken = JSON.parse(localStorage.getItem("API_TOKEN")).access_token;
+
+    axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId', {
+      params: {
+        'nic': `${newid}`
+      },
+
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+
+      }
+
+
+
     }).then((response) => {
-      if(response.data.body=='true'){
+      if (response.data.body == 'true') {
         setState(1);
-        axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/policeccheck/1.0.0/getalldetails',{ 
-          params:{
-           'nic':`${newid}`}, 
-           
-           headers: {
-               'Authorization': `Bearer ${accessToken}`,
-             
-           }
-   
-   
-        
-       }).then((response)=>{
-console.log(response.data.body)
-       });
+        axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/policeccheck/1.0.0/getalldetails', {
+          params: {
+            'nic': `${newid}`
+          },
 
-        
+          headers: {
+            'Authorization': `Bearer ${accessToken}`,
+
+          }
+
+
+
+        }).then((response) => {
+          console.log(response.data.body)
+        });
+
+
       }
-      else{
-        
+      else {
+
       }
-   console.log(response.data.body)
-  })
 
-   
-  
-    }
-    return (
-        <>
-            <TopBar />
-            <div className="home">
-                <div className='content'>
-                    <div className='contentOne'>
-
-                        {/* <label for="fname" className='labelnic'>Please Enter your nic</label> */}
-                        <br></br>
-                        <input type="text" id="fname" name="fname" onChange={changenic} value={nic} className='nicInput' placeholder='Please Enter your nic' />
-                        <button onClick={submitID} className='nicBut'>Next</button>
-                        <div className='stepsDiv'>
-                            <Steps current={statestep} currentStatus={currentStatus}>
-                                <Steps.Item title="Identity Check"/>
-                                <Steps.Item title="Police Check" className='steps'/>
+    })
 
 
-                            </Steps>
-                        </div>
-                    </div>
 
-                    <div className='contentOne'>
-                        <SideNIC />
-                    </div>
-                </div>
+  }
+  return (
+    <>
+      <TopBar />
+      <div className="home">
+        <div className='content'>
+          <div className='contentOne'>
+
+            {/* <label for="fname" className='labelnic'>Please Enter your nic</label> */}
+            <br></br>
+            <input type="text" id="fname" name="fname" onChange={changenic} value={nic} className='nicInput' placeholder='Please Enter your nic' />
+            <button onClick={submitID} className='nicBut'>Next</button>
+            <div className='stepsDiv'>
+              <Steps current={statestep} currentStatus={currentStatus}>
+                <Steps.Item title="Identity Check" />
+                <Steps.Item title="Police Check" className='steps' />
+
+
+              </Steps>
             </div>
-        </>
-    );
+          </div>
+
+          <div className='contentOne'>
+            <SideNIC />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
