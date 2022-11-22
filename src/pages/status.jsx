de87c56605c158c3b54775daf03436097cd03fd9
mtbstn24 <thushibstn24@gmail.com>
2022-11-22 13:09:21 +0530

    check status

--- src/pages/status.jsx
+++ src/pages/status.jsx
@@ -49,7 +49,7 @@ export default function Status() {
       let res=  axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId?',{
         params: {
           // 'nic': `${newid}`
-          'nic':'9'
+          'nic':'string'
         },
 
         headers: {
