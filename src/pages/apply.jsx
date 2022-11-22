524e631cf4df1e7a867acda6863f730f29a5f373
malithiperera <malithiperera1998@gmail.com>
2022-11-22 15:52:20 +0530

    updte

--- src/pages/apply.jsx
+++ src/pages/apply.jsx
@@ -51,7 +51,7 @@ export default function Apply() {
 
 
       const accessToken = JSON.parse(localStorage.getItem("API_TOKEN")).access_token;
-      axios.post('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/policeccheck/1.0.0/getalldetails', {
+      axios.post('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/addresscheck/1.0.0/addRequest', {
         params: {
           'nic': `${nics}`,
           'address':`${add1}+ " " ${add2}`,
