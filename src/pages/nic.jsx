9dc63f3c59ce193a2604b76a2898d49e3dbadb1e
Haathim_wso2 <haathimmunas@gmail.com>
2022-11-22 14:49:48 +0530

    .

--- src/pages/nic.jsx
+++ src/pages/nic.jsx
@@ -46,8 +46,8 @@ export default function NIC(props) {
     console.log("Testing 2", state.email)
     const accessToken = JSON.parse(localStorage.getItem("API_TOKEN")).access_token;
 
-    checkTokenAndRenew().then(() => {
-    return axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId', {
+
+      axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/identitycheck/1.0.0/checkId', {
       params: {
         'nic': `${newid}`
       },
@@ -59,7 +59,6 @@ export default function NIC(props) {
 
 
 
-    })
     }).then((response) => {
       console.log("ID Check", response.data.body)
       if (response.data.body == 'true') {
