7d9d38b915d8ca109201ce42370a32fb87989288
malithiperera <malithiperera1998@gmail.com>
2022-11-23 18:05:31 +0530

    new ha

--- src/pages/apply.jsx
+++ src/pages/apply.jsx
@@ -76,6 +76,18 @@ export default function Apply() {
     console.log(nics,add1,add2,imgURL,tpnumber,state.email)
 
     const accessToken = JSON.parse(localStorage.getItem("API_TOKEN")).access_token;
+    axios.get('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/gramaconnect/1.0.0/requestavailable', {
+      params: {
+        'nic': `${nics}`
+      },
+
+      headers: {
+        'Authorization': `Bearer ${accessToken}`,
+
+      }
+    }).then((response)=>{
+console.log(response.data)
+    })
     axios.post('https://7fa2c1a4-2bfc-4c58-899f-9569c112150b-prod.e1-us-east-azure.choreoapis.dev/ddrq/addresscheck/1.0.0/addRequest', {
     
         'nic': `${nics}`,
