37ad05fc0222a3825901ce2814bb3637a379b130
mtbstn24 <thushibstn24@gmail.com>
2022-11-24 10:09:28 +0530

    non login redirection

--- src/pages/options.jsx
+++ src/pages/options.jsx
@@ -4,8 +4,7 @@ import Side from '../components/side';
 
 
 import './index.css'; 
-import { Link } from 'react-router-dom';
-
+import { Link , useHistory} from 'react-router-dom';
 
 
 export default function Options() {
